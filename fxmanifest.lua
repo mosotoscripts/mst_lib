fd1035ff343ed1df84c17cee2090e0c3dadef200
Georgi Stoyanov <stoyanow2707@gmail.com>
2025-03-18 13:50:00 +0200

    fxmanifest added

---
+++ fxmanifest.lua
@@ -0,0 +1,33 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
+
+name 'mst_lib'
+author 'te6ka_ninja'
+version '0.0.1'
+description 'A FiveM library which helps handle some of the Mosoto Scripts key features.'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'handlers/shared.lua',
+    'handlers/**/shared.lua'
+}
+
+client_scripts {
+    'handlers/**/client.lua'
+}
+
+server_scripts {
+    'handlers/**/server.lua'
+}
+
+files {
+    'api.lua',
+    'modules/**/*'
+}
+
+dependencies {
+    '/server:7290',
+    'ox_lib'
+}
