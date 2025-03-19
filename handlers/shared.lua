34ed86bf4256aea75218a75ace016cb546a787f0
Georgi Stoyanov <stoyanow2707@gmail.com>
2025-03-19 10:56:04 +0200

    feat: enhance mst table with debug info and export functionality

--- handlers/shared.lua
+++ handlers/shared.lua
@@ -1,4 +1,13 @@
 -- Based on https://github.com/overextended/ox_lib/blob/master/resource/init.lua
 
-local mst = {}
-_ENV.mst = mst
+local debug_getinfo = debug.getinfo
+
+_ENV.mst = setmetatable({}, {
+    __newindex = function(self, name, fn)
+        rawset(self, name, fn)
+
+        if debug_getinfo(2, 'S').short_src:find('@mst_lib/handlers') then
+            exports(name, fn)
+        end
+    end
+})
